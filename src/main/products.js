bc575c761b229e598147128a2869bc58cbf0aa0f
Garrett Moore <34557347+gctm808@users.noreply.github.com>
2019-05-10 17:06:14 -0700

    continue products page

--- src/main/products.js
+++ src/main/products.js
@@ -5,7 +5,7 @@ import ProductBox from '../components/molecules/productBox';
 
 const Products = () => {
     return (
-        <div className='l'>
+        <div className='l extraPadding'>
             <h2>FULLY CUSTOMIZED SERVICE</h2>
             <p>We pride ourselves in keeping business friendly and light and provide our brokers with competitive commission as well as the attentiveness needed to further their individual success. Being a boutique agency, we know each broker and company by name, so we can better serve our clients’ needs.</p>
             <h2>WHAT WE OFFER</h2>
