177a2cc4a34bed9354a3e8007f727371485cb72b
Garrett Moore <34557347+gctm808@users.noreply.github.com>
2019-05-20 17:17:56 -0700

    add product pages

---
+++ src/main/products/life.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Life = () => {
+    return (
+        <div>
+            <div>
+                <h2>Establishing a Solid Simple Foundation</h2>
+                <p>Our life-long passion has been to help families establish solid financial foundations, starting with life insurance and offering long-term and personalized answers. Therefore, we unceasingly explore options that will complete our reputable life insurance portfolio, which include but are not limited to:</p>
+                <ul>
+                    <li>Universal Life</li>
+                    <li>Whole Life</li>
+                    <li>Term Life</li>
+                    <li>Survivorship Life</li>
+                    <li>Indexed Life</li>
+                    <li>Impaired Life</li>
+                    <li>Guarantee and simplified issue</li>
+                    <li>Small group life</li>
+                </ul>     
+                <div>
+                    <iframe title='lifeVid' src="//lifehappenspro.org/resources/embeded?userId=27409&videoId=5834859709001" width="360" height="371" scrolling="no" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe>
+                </div>
+                <h3>Life Insurance Calculators:</h3>
+                <p>These life insurance calculators that will help to determine your life insurance needs:</p>
+            </div>
+            <div>
+                <div>
+                    <iframe title='calc1' src="https://widgets.memberedge.io/planner/?cHJpbWFyeT04RkJDOEYmc2Vjb25kYXJ5PWZmYmEwMCZ0ZXh0PWZmZmZmZiZoaWdobGlnaHQ9ZmZiYTAwJmxpZmU9aHR0cDovL3d3dy5saWZlaGFwcGVucy5vcmcvaW5zdXJhbmNlLW92ZXJ2aWV3L2xpZmUtaW5zdXJhbmNlLyZkaXNhYmlsaXR5PWh0dHA6Ly93d3cubGlmZWhhcHBlbnMub3JnL2luc3VyYW5jZS1vdmVydmlldy9kaXNhYmlsaXR5LWluc3VyYW5jZS8mbG9uZ3Rlcm09aHR0cDovL3d3dy5saWZlaGFwcGVucy5vcmcvaW5zdXJhbmNlLW92ZXJ2aWV3L2Rpc2FiaWxpdHktaW5zdXJhbmNlLyZ1c2VyPTI3NDA5JnR5cGU9cGxhbm5lcg==" border="0" scrolling="no"></iframe>
+                </div> 
+                <div>
+                    <iframe title='calc2' src="https://widgets.memberedge.io/calculators/?cHJpbWFyeT1mZmJhMDAmc2Vjb25kYXJ5PThGQkM4RiZ0ZXh0PWZmZmZmZiZoaWdobGlnaHQ9ZmZiYTAwJmxpZmU9aHR0cDovL3d3dy5saWZlaGFwcGVucy5vcmcvaW5zdXJhbmNlLW92ZXJ2aWV3L2xpZmUtaW5zdXJhbmNlLyZkaXNhYmlsaXR5PWh0dHA6Ly93d3cubGlmZWhhcHBlbnMub3JnL2luc3VyYW5jZS1vdmVydmlldy9kaXNhYmlsaXR5LWluc3VyYW5jZS8mbG9uZ3Rlcm09aHR0cDovL3d3dy5saWZlaGFwcGVucy5vcmcvaW5zdXJhbmNlLW92ZXJ2aWV3L2Rpc2FiaWxpdHktaW5zdXJhbmNlLyZ1c2VyPTI3NDA5JnR5cGU9Y2FsY3VsYXRvcg==" border="0" scrolling="no"></iframe>
+                </div>
+            </div>
+            <div>
+                <p>For agents seeking a quote, click below</p>
+                <a href="http://advisor.allegisag.com/forms/life-insurance-illustration-request" target="_blank" rel='noopener noreferrer'>Life Insurance Quote</a>     
+            </div>
+        </div>
+    );
+};
+
+export default Life;
