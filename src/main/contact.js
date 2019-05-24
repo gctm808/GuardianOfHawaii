529bd311f871445200aedf2ed2b21e0963f615fa
Garrett Moore <34557347+gctm808@users.noreply.github.com>
2019-05-24 16:33:00 -0700

    start contact page, fix footer

--- src/main/contact.js
+++ src/main/contact.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../css/styles.css';
+import Form from '../components/organisms/form';
 
 const Contact = () => {
     return (
-        <div className='l'>
-            contact
+        <div className='l extraPadding'>
+            <h2>GET IN TOUCH</h2>
+            <h4>Please fill out the form below and we will in touch as soon as possible.</h4><br />
+            <div className='l--two-col'>
+                <div className='l-main'>
+                    <Form field1='Name' field2='Email' field3='Message' button='SUBMIT' />
+                </div>
+                <div className='l-sidebar'>
+                    sidebar
+                </div>
+            </div>
         </div>
     );
 };
