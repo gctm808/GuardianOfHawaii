376ed44ed30573a107efc20d98d9737910f79e13
Garrett Moore <34557347+gctm808@users.noreply.github.com>
2019-05-31 16:12:01 -0700

    add scrolltotop component, animate landing

--- src/main/home.js
+++ src/main/home.js
@@ -4,6 +4,7 @@ import landing from '../images/landing.jpg';
 import Description from './landing/description';
 import About from './landing/about';
 import Products from './landing/products';
+import '../css/styles.css';
 
 const Home = () => {
     return (
@@ -15,9 +16,9 @@ const Home = () => {
             backgroundRepeat: 'no-repeat'}} className='landing'>
                 <div className='l divider'>
                     <div className='headers'>
-                        <h1>YOUR CLIENTS COME FIRST</h1>
-                        <h2 style={{lineHeight: "1.5"}}>Guardian of Hawaii. <span style={{fontWeight: "lighter"}}>Helping you protect <br />what matters most to your clients.</span></h2>
-                        <a href='https://lifepipe.ipipeline.com/LTSearch.aspx?r=1490547670704388677&GAID=5229' target='_blank' rel='noopener noreferrer'><Button name='RUN MY QUOTE'/></a>
+                        <h1 className='transitionTop'>YOUR CLIENTS COME FIRST</h1>
+                        <h2 className='transitionLeft' style={{lineHeight: "1.5"}}>Guardian of Hawaii. <span style={{fontWeight: "lighter"}}>Helping you protect <br />what matters most to your clients.</span></h2>
+                        <div className='transitionBottom'><a href='https://lifepipe.ipipeline.com/LTSearch.aspx?r=1490547670704388677&GAID=5229' target='_blank' rel='noopener noreferrer'><Button name='RUN MY QUOTE'/></a></div>
                     </div>
                 </div>
             </div>
